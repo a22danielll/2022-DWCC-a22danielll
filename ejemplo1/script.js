a891e442c9dc214efc1b08c47062c69d50516a6a
a22danielll <a22danielll@iessanclemente.net>
2022-09-23 10:27:08 +0200

    pruebas en javascript 2

--- ejemplo1/script.js
+++ ejemplo1/script.js
@@ -1,3 +1,10 @@
 var texto = 'hello';
 document.write(texto);
 console.log('Mensaje');
+console.warn('Mensaje de aviso');
+console.error('Mensaje de error');
+alert('alerta');
+
+//mensaje pront
+let person = prompt('Mensaje pront', 'prueba');
+//incompleto
