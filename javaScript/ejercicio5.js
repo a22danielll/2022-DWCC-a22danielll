0bb178a167e56667443b0f8487d9f91e6ef8b424
a22danielll <a22danielll@iessanclemente.net>
2022-10-03 16:45:21 +0200

    ejercicio5

--- javaScript/ejercicio5.js
+++ javaScript/ejercicio5.js
@@ -1,2 +1,10 @@
 let randomNum = Math.floor(Math.random() * 100);
 console.log(randomNum);
+let num = prompt('Introduce un número:');
+if (randomNum > num) {
+  console.log('el número introducido que menor ');
+} else if (randomNum < num) {
+  console.log('el numero introducido es mayor');
+} else {
+  console.log('los numeros son iguales');
+}
---
+++ javaScript/ejercicio6.js
@@ -0,0 +1,5 @@
+//Pide un número por teclado ao usuario. Calcula o factorial dese número e mostra a resultado por consola. (5! = 5*4*3*2*1).
+let numero = prompt('numero a intriducir');
+
+numero = numero * (numero - 1);
+console.log(numero);
