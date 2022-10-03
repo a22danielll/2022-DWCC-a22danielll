61b62f51dc0c7c2ab272d5ae2a7c4bc8f91d47ba
a22danielll <a22danielll@iessanclemente.net>
2022-10-03 17:48:27 +0200

    ejercicio7

--- javaScript/ejercicio6.js
+++ javaScript/ejercicio6.js
@@ -1,5 +1,7 @@
 //Pide un número por teclado ao usuario. Calcula o factorial dese número e mostra a resultado por consola. (5! = 5*4*3*2*1).
 let numero = prompt('numero a intriducir');
-
-numero = numero * (numero - 1);
-console.log(numero);
+let res = 1;
+for (i = 1; i <= numero; i++) {
+  res = res * i;
+}
+console.log(res);
---
+++ javaScript/ejercicio7.js
@@ -0,0 +1,5 @@
+/*Pide un número por teclado ao usuario. Despois crea unha variable de tipo array e asígnalle a lista de números enteiros dende o 0 ao número tecleado polo usuario.
+Ademais, fai a suma de todos os valores contidos no array e mostra por consola o resultado da suma.*/
+
+let nUsuario = prompt('numero a intriducir');
+let arr = Array();
