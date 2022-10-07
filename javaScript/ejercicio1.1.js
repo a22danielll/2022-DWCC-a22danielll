5b3a43fa2ae63d896f6c686990fb04e4b2e71d8c
a22danielll <a22danielll@iessanclemente.net>
2022-10-07 10:27:33 +0200

    ejercicio 1.1 avanzando

--- javaScript/ejercicio1.1.js
+++ javaScript/ejercicio1.1.js
@@ -29,8 +29,35 @@ console.log(sumar(5, 7, 9));
 
 //4. Crea unha función á que se lle pasen varios números como parámetros (un número indeterminado de parámetros). Debe devolver a media dos números pasados. Proba a realizala con diferentes bucles for (con contador, for…in e for…of).
 
-//5. Fai unha función á que se lle pase un DNI (ex: 12345678w ou 87654321T) e devolva se é correcto ou non.
+function promedio(...numero) {
+  let media = 0;
+  let suma = 0;
+  for (const arg of numero) {
+    suma += arg;
+    media = suma / numero.length;
+  }
+  return media;
+}
 
+console.log(promedio(5, 5, 5));
+
+//5. Fai unha función á que se lle pase un DNI (ex: 12345678w ou 87654321T) e devolva se é correcto ou non.
+let numero = /[0-9]{8}/;
+let letraCondicion = /[A-Z]{1}/;
+let num = 0;
+let letra = '';
+function validarDNI(num, letra) {
+  if (numero.test(num) == true) {
+    if (letraCondicion.test(letra) == true) {
+      console.log('el dni es correcto');
+    } else {
+      console.log('incorrecto');
+    }
+  } else {
+    console.log('incorrecto');
+  }
+}
+console.log(validarDNI(442112, 'P'));
 //6. Crea unha función que reciba un array bidimensional de lonxitude variable que se corresponda cun escenario do xogo de Buscaminas. Este array almacenará un -1 nas posicións onde hai minas e un 0 en caso contrario. A función debe devolver un array bidimensional onde cada posición que non teña mina, debe ter a información do número de minas adxacentes (diagonal, horizontal e vertical).
 //7. Crea unha función JavaScript que comprobe se é poxible axendar unha reunión dentro do horario laboral.
 //8. Crea unha función chamada buscarPatron(texto, patron) que reciba como parámetros un texto e un patrón. A función debe devolver como resultado o número de veces que aparece o patrón no texto.
