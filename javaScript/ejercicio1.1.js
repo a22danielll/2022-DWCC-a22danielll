cc09931bec5636f2deaff565d5388f3342d9971f
a22danielll <a22danielll@iessanclemente.net>
2022-10-11 12:06:00 +0200

    ejercicio 1.1 subido

--- javaScript/ejercicio1.1.js
+++ javaScript/ejercicio1.1.js
@@ -67,22 +67,43 @@ arrEntrada = [
   [0, 0, -1, 0],
   [0, -1, -1, 0],
 ];
-arraySaida = [
-  [1, 3, -1, 2],
-  [1, -1, -1, 2],
-];
+
+function calcularMinas(taboleiro) {
+  const arraySalidas = [];
+  for (fila = 0; fila < taboleiro.length; fila++) {
+    arraySalidas[fila] = [];
+    for (let columna = 0; columna < taboleiro[fila].length; columna++) {
+      if (taboleiro[fila][columna] === -1) {
+        arraySalidas[fila][columna] = -1;
+      }
+    }
+  }
+  return arraySalidas;
+}
+console.log(calcularMinas(arrEntrada));
+
 //7. Crea unha función JavaScript que comprobe se é poxible axendar unha reunión dentro do horario laboral.
 ('use strict');
 const inicioXornada = '07:30';
 const finalXornada = '17:45';
 
 function axendarReunion(horaInicioReunion, duracionEnMinutos) {
-  total = '';
+  const value = horaInicioReunion.split(':');
+  const inicio = value[0] * 60;
+  const inicio2 = value[1] * 60;
+  const t = inicio + inicio2;
+  const total = t + duracionEnMinutos;
+  console.log(
+    'inicio: ' + inicio,
+    inicio2,
+    ' inicio reunion: ' + t + ' ' + total
+  );
+
   //split de horasinicioreunion
   //saca u array y el array se iguala a la posicion de horas y minutos se pasa a minutos y se hace la suma
-  console.log('total:' + total);
-  if (total >= inicioXornada && total <= finalXornada) {
-    return false;
+
+  if (total < inicioXornada && total > finalXornada) {
+    return flase;
   } else {
     return true;
   }
@@ -114,5 +135,84 @@ console.log(
 
 //8. Crea unha función chamada buscarPatron(texto, patron) que reciba como parámetros un texto e un patrón. A función debe devolver como resultado o número de veces que aparece o patrón no texto.
 
-function buscarPatron(texto, patron) {}
+function buscarPatron(texto, patron) {
+  let coincidente = 0;
+  let indice = 0;
+  let coindice = true;
+
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] === patron[indice]) {
+      indice = i;
+      coincide = true;
+      for (let x = 0; x < patron.length; x++) {
+        if (patron[x] !== texto[indice]) {
+          coincide = false;
+          break;
+        }
+      }
+      indice++;
+    }
+    if (coindice) {
+      coincidente++;
+    }
+  }
+  return indice;
+}
+console.log(buscarPatron('000111101000ABCHO', '00'));
 //9.  Crea unha función que reciba como parámetro unha cantidade enteira e faga o desglose do número de billetes e moedas necesarios para obtela. Debe usarse o número mínimo de billetes e moedas.
+function cambio(cantidad) {
+  let moneda1 = 1;
+  let moneda2 = 2;
+  let billete5 = 5;
+  let billete10 = 10;
+  let billete20 = 20;
+  let billete50 = 50;
+  let billete100 = 100;
+  let billete200 = 200;
+  let billete500 = 500;
+
+  let resultado = '';
+  if (cantidad / billete500 >= 1) {
+    while (cantidad / billete500 >= 1) {
+      return (resultado = cantidad / billete500);
+    }
+  } else if (cantidad / billete200 >= 1) {
+    while (cantidad / billete200 >= 1) {
+      return (resultado = cantidad / billete200);
+    }
+  } else if (cantidad / billete100 >= 1) {
+    while (cantidad / billete100 >= 1) {
+      return (resultado = cantidad / billete100);
+    }
+  } else if (cantidad / billete50 >= 1) {
+    while (cantidad / billete50 >= 1) {
+      return (resultado = cantidad / billete50);
+    }
+  } else if (cantidad / billete20 >= 1) {
+    while (cantidad / billete20 >= 1) {
+      return (resultado = cantidad / billete20);
+    }
+  } else if (cantidad / billete10 >= 1) {
+    while (cantidad / billete10 >= 1) {
+      return (resultado = cantidad / billete10);
+    }
+  } else if (cantidad / billete5 >= 1) {
+    while (cantidad / billete5 >= 1) {
+      return (resultado = cantidad / billete5);
+      console.log('Devolveremos' + resultado + 'billetes de 5');
+    }
+  } else if (cantidad / moneda2 >= 1) {
+    while (cantidad / moneda2 >= 1) {
+      return (resultado = cantidad / moneda2);
+      console.log('Devolveremos' + resultado + 'monedas de 2');
+    }
+  } else if (cantidad / moneda1 >= 1) {
+    while (cantidad / moneda1 >= 1) {
+      return (resultado = cantidad / moneda1);
+      console.log('Devolveremos' + resultado + 'monedas de 1');
+    }
+  }
+  return 'devuelve' + resultado;
+}
+
+console.log(cambio(35));
