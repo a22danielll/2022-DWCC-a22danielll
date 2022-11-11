5a53b6d704c01e032c8d210240500b2bb1fba50a
a22danielll <a22danielll@sanclemente.net>
2022-11-11 22:15:38 +0100

    ejercicios dom comenzando

---
+++ DOM/ejercicio.js
@@ -0,0 +1,8 @@
+
+
+rtrtrtrtrtrtrconsole.log('A táboa con id="age-table".');
+console.log('Todos os elementos label dentro da táboa (debería haber 3).');
+console.log(' O primeiro td da táboa.');
+console.log(' O form con name="search".');
+console.log('O primeiro input do formulario anterior formulario.');
+console.log(' O último input do formulario anterior formulario.');
