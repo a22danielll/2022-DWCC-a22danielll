1444adc285feb46a1f48a07837babd9c3e179085
a22danielll <a22danielll@sanclemente.net>
2022-11-12 20:52:18 +0100

    ejercicios dom

--- DOM/02-accesoNodos01.js
+++ DOM/02-accesoNodos01.js
@@ -1,7 +1,18 @@
 console.log('A táboa con id="age-table".');
 document.body.getElementsByTagName("table")[0];
+document.getElementById("age-table");
+
 console.log('Todos os elementos label dentro da táboa (debería haber 3).');
+document.body.getElementsByTagName("label");
+
 console.log(' O primeiro td da táboa.');
+
 console.log(' O form con name="search".');
+document.getElementsByName("search")[0];
+
 console.log('O primeiro input do formulario anterior formulario.');
+document.getElementsByTagName("input")[0];
+
 console.log(' O último input do formulario anterior formulario.');
+document.getElementsByTagName("input")[8];
+
