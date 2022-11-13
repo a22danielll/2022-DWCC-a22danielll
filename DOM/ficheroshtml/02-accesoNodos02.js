3e732fe0b040e8ebbf87508a44a9c9bc73736139
a22danielll <a22danielll@sanclemente.net>
2022-11-13 20:25:49 +0100

    ejercicios dom

--- DOM/ficheroshtml/02-accesoNodos02.js
+++ DOM/ficheroshtml/02-accesoNodos02.js
@@ -2,8 +2,10 @@ console.log(
   "2. Descarga o código fonte 02-accesoNodos02.html e indica, polo menos, unha forma de acceder aos seguintes nodos:"
 );
 console.log("● O elemento con id “input2”.");
+document.getElementById("input2");
 console.log("● A colección de parágrafos");
 console.log("● Todos os parágrafos dentro do div con id “lipsum”.");
+
 console.log("● O formulario");
 console.log("● Todos os inputs");
 console.log("● Só os inputs con nome “sexo”.");
