629e4b8be257a750de0f8d868e732d1603a74de6
a22danielll <a22danielll@iessanclemente.net>
2022-11-14 17:54:00 +0100

    ejercicios dom

--- DOM/ficheroshtml/03-propiedadesNodo01.js
+++ DOM/ficheroshtml/03-propiedadesNodo01.js
@@ -1,9 +1,11 @@
 console.log(
-  "1. Descarga o código fonte 03-propiedadesNodo01.html e indica, polo menos, unha forma de acceder ao seguinte contido:"
+  '1. Descarga o código fonte 03-propiedadesNodo01.html e indica, polo menos, unha forma de acceder ao seguinte contido:'
 );
-console.log("● O innerHTML da etiqueta “Escolle sexo”:");
-console.log("● O textContent da etiqueta anterior");
-console.log("● O valor do primeiro input de sexo");
-console.log("● O valor do sexo que estea seleccionado.");
-console.log("● O texto de cada un dos elementos <li>");
-console.log("● Indica cantos elementos <li> hai.");
+console.log('● O innerHTML da etiqueta “Escolle sexo”:');
+console.log((document.innerHTML = 'Escolle o sexo:'));
+console.log('● O textContent da etiqueta anterior');
+console.log('● O valor do primeiro input de sexo');
+console.log('● O valor do sexo que estea seleccionado.');
+console.log('● O texto de cada un dos elementos <li>');
+console.log('● Indica cantos elementos <li> hai.');
+console.log(document.getElementsByTagName('li').length);
