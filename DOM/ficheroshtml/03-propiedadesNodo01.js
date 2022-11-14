0488cd3b9c55bb3c00d39ebdf691432b624b2bba
a22danielll <a22danielll@iessanclemente.net>
2022-11-14 17:56:18 +0100

    ejercicios dom

--- DOM/ficheroshtml/03-propiedadesNodo01.js
+++ DOM/ficheroshtml/03-propiedadesNodo01.js
@@ -7,5 +7,6 @@ console.log('● O textContent da etiqueta anterior');
 console.log('● O valor do primeiro input de sexo');
 console.log('● O valor do sexo que estea seleccionado.');
 console.log('● O texto de cada un dos elementos <li>');
+console.log(document.getElementsByTagName('li').textContent);
 console.log('● Indica cantos elementos <li> hai.');
 console.log(document.getElementsByTagName('li').length);
