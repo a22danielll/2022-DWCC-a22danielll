36e790a403bf0abbf3c30aa93903498757b21390
a22danielll <a22danielll@iessanclemente.net>
2022-11-21 17:11:11 +0100

    ejercicio dom

--- DOM/ficheroshtml/03-propiedadesNodo02.js
+++ DOM/ficheroshtml/03-propiedadesNodo02.js
@@ -1,13 +1,29 @@
-console.log("2. Descarga o código fonte 03-propiedadesNodo02.html e indica:");
+console.log('2. Descarga o código fonte 03-propiedadesNodo02.html e indica:');
 
-console.log("● O número de ligazóns da páxina.");
-console.log(document.getElementsByTagName("a").length);
-console.log("● A dirección da penúltima ligazón.");
-
-console.log("● O número de ligazóns que apuntan a http://proba");
-
-console.log("● O número de ligazóns do terceiro parágrafo.");
-
+console.log('● O número de ligazóns da páxina.');
+let ligazon = document.getElementsByTagName('a');
+console.log(ligazon.length);
+console.log('● A dirección da penúltima ligazón.');
+let ligazon2 = document.getElementsByTagName('a');
+console.log(ligazon2[ligazon2.length - 1].href);
+console.log('● O número de ligazóns que apuntan a http://proba');
+let contador = 0;
+for (var i = 0; i < ligazon.length; i++) {
+  if (ligazon[i].href == 'http://proba/') {
+    contador++;
+  }
+}
+console.log(contador);
+console.log('● O número de ligazóns do terceiro parágrafo.');
+let ligazon4 = document
+  .getElementsByTagName('p')[2]
+  .getElementsByTagName('a').length;
+console.log(ligazon4);
 console.log(
-  "● Fai que as ligazóns que apuntan a http://proba teñan o texto de cor laranxa."
+  '● Fai que as ligazóns que apuntan a http://proba teñan o texto de cor laranxa.'
 );
+for (var i = 0; i < ligazon.length; i++) {
+  if (ligazon[i].href) {
+    ligazon[i].style.color = '#FF8000';
+  }
+}
