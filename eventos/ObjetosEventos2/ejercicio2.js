41fdd96fc47abc76a94379fd869619a2added28f
a22danielll <a22danielll@iessanclemente.net>
2022-11-28 17:19:01 +0100

    ejercicio 2 eventos

--- eventos/ObjetosEventos2/ejercicio2.js
+++ eventos/ObjetosEventos2/ejercicio2.js
@@ -2,16 +2,16 @@ console.log(
   '2. Dado o seguinte código HTML, engade a configuración CSS para que inicialmente o span estea oculto. Ao pulsar a ligazón debe facerse visible o contido do span e desaparecer a ligazón.'
 );
 let texto = document.getElementById('textoExercicio2');
-let span = document.getElementById('adicional');
-let ligazons = document.getElementById('ligazon');
 
-function mostrarTexto() {}
+let span = document.getElementById('adicional');
+let ligazon = document.getElementById('ligazon');
 
 function eliminarLigazon() {
-  ligazon.remove();
-}
+  //span.className = 'visible';
 
-for (let eliminar of texto) {
-  eliminar.addEventListener('click', mostrarTexto);
-  eliminar.addEventListener('click', eliminarLigazon);
+  const classes = span.classList;
+  classes.remove('oculto');
+
+  ligazon.hidden = 'true';
 }
+document.addEventListener('click', eliminarLigazon);
