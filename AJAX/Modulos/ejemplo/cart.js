5f84aa240b7771621fc0b5a216a8917d5fcfc31c
a22danielll <a22danielll@iessanclemente.net>
2023-02-09 09:48:36 +0100

    s

---
+++ AJAX/Modulos/ejemplo/cart.js
@@ -0,0 +1,8 @@
+console.log('Exporting module');
+const shippingCost = 10;
+export const cart = [];
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+export default shippingCost;
