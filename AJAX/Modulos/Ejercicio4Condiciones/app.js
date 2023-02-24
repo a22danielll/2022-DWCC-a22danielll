04b78bb1230822a9641ba1face0b214f1de5fbef
a22danielll <a22danielll@iessanclemente.net>
2023-02-24 10:24:54 +0100

    s

---
+++ AJAX/Modulos/Ejercicio4Condiciones/app.js
@@ -0,0 +1,32 @@
+const game = Vue.createApp({
+  data() {
+    return {
+      vidaMonstro: 100,
+      vidaVostede: 100,
+    };
+  },
+
+  methods: {
+    ataque() {
+      this.vidaMonstro =
+        this.vidaMonstro - Math.floor(Math.random() * (12 - 5) + 5);
+      this.vidaVostede =
+        this.vidaVostede - Math.floor(Math.random() * (15 - 8) + 8);
+    },
+    ataqueEspecial() {
+      this.vidaMonstro =
+        this.vidaMonstro - Math.floor(Math.random() * (25 - 10) + 10);
+
+      this.vidaVostede =
+        this.vidaVostede - Math.floor(Math.random() * (15 - 8) + 8);
+    },
+    curacion() {
+      if (this.vidaVostede >= 100) {
+        console.log('ya tienes todo la vida al completo');
+      } else if (this.vidaVostede < 100) {
+        this.vidaVostede =
+          this.vidaVostede + Math.floor(Math.random() * (20 - 8) + 8);
+      }
+    },
+  },
+}).mount('#game');
