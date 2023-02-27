98dd4e22d1774f2aed3f7dcf6b7b98dba390fae3
a22danielll <a22danielll@iessanclemente.net>
2023-02-27 17:58:14 +0100

    s

---
+++ AJAX/Componentes/ejercicio1ComponentesHijo/src/components/Boton.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { ref } from 'vue';
+</script>
+
+<template>
+  <button type="button">Mayusculas</button>
+</template>
+
+<style scoped>
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ AJAX/Componentes/vite-proyect/src/components/childComponent.vue
@@ -0,0 +1,9 @@
+<script>
+  export default {
+    // Escribir as opcións do compoñente: data, methods, watch, … en caso necesario
+    //data() {...}
+  };
+</script>
+<template>
+  <h2>A Child Component!</h2>
+</template>
