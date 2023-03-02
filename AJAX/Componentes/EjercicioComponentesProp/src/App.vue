05b6e41c7d8dff96e253dcaec0989e044847f235
a22danielll <a22danielll@iessanclemente.net>
2023-03-02 11:19:47 +0100

    s

--- AJAX/Componentes/EjercicioComponentesProp/src/App.vue
+++ AJAX/Componentes/EjercicioComponentesProp/src/App.vue
@@ -1,30 +1,64 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  //import HelloWorld from './components/HelloWorld.vue';
+
+  import Contactos from './components/Contactos.vue';
+  export default {
+    // Escribir as opcións do compoñente: data, methods, watch, ...
+    data() {
+      return {
+        contactos: [
+          {
+            name: 'daniel',
+            num: 693624694,
+            email: 'daniel@gmail.com',
+            mostrar: true,
+          },
+          {
+            name: 'martin',
+            num: 693693693,
+            email: 'martin@gmail.com',
+            mostrar: true,
+          },
+        ],
+        mostrar: true,
+      };
+    },
+    // rexistro do compoñente
+    components: {
+      Contactos,
+    },
+    props: {
+      name: String,
+      num: Number,
+      email: String,
+      mostrar: Boolean,
+    },
+  };
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!--<HelloWorld msg="Ola mundo" />-->
+
+  <Contactos
+    v-for="contacto in contactos"
+    :name="contacto.name"
+    :num="contacto.num"
+    :email="contacto.email"
+  ></Contactos>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 </style>
---
+++ AJAX/Componentes/EjercicioComponentesProp/src/components/Contactos.vue
@@ -0,0 +1,59 @@
+<script>
+  import { ref } from 'vue';
+
+  export default {
+    data() {
+      return {
+        msg: 'ejercicio1',
+        contactos: [
+          {
+            name: 'daniel',
+            num: '693624694',
+            email: 'daniel@gmail.com',
+            mostrar: true,
+          },
+          {
+            name: 'martin',
+            num: '693693693',
+            email: 'martin@gmail.com',
+            mostrar: true,
+          },
+        ],
+        mostrar: true,
+      };
+    },
+    methods: {
+      ocultarMostrar() {
+        if (this.mostrar == true) {
+          this.mostrar = false;
+        } else {
+          this.mostrar = true;
+        }
+      },
+    },
+    /*props: {
+      name: String,
+      num: Number,
+      email: String,
+      mostrar: Boolean
+    },*/
+  };
+</script>
+
+<template>
+  <h3>{{ msg }}</h3>
+
+  <div>
+    <h4 v-for="contacto in contactos">
+      <h4>{{ contacto.name }}</h4>
+      <div v-if="mostrar">
+        {{ contacto.num }}
+        {{ contacto.email }}
+      </div>
+
+      <button @click="ocultarMostrar">{{ mostrar }}</button>
+
+      <button @click="ocultarMostrar">ocultar/mostrar</button>
+    </h4>
+  </div>
+</template>
