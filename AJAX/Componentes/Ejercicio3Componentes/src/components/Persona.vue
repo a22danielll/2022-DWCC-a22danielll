40d1ae9cc82deb5bcba67318df4ded1b89f660be
a22danielll <a22danielll@iessanclemente.net>
2023-03-14 10:26:24 +0100

    s

---
+++ AJAX/Componentes/Ejercicio3Componentes/src/components/Persona.vue
@@ -0,0 +1,22 @@
+<script>
+  import { ref } from 'vue';
+  export default {
+    data() {
+      return {};
+    },
+    methods: {
+      /*anhadir() {
+        this.$emit('nuevo', this.id, this.name, this.num, this.email);
+      },*/
+    },
+    $emit: ['nuevo'],
+  };
+</script>
+
+<template></template>
+
+<style scoped>
+  .read-the-docs {
+    color: #888;
+  }
+</style>
